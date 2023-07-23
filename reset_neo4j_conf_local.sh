4e3bafff8f955858951f162db79d5bb9ad88e3da
udothemath <fuchang.sun@gmail.com>
2023-07-23 22:01:45 +0800

    fix: fix  typo in  reset_neo4j_conf_loca

--- reset_neo4j_conf_local.sh
+++ reset_neo4j_conf_local.sh
@@ -1,25 +1,9 @@
 echo "Hi. Welcome aboard!"
-PATH=/Users/pro/neo4j-community-4.4.23
-echo "File path: $PATH" && echo "OK" || echo "NOK"
-echo "hi" && echo "OK" || echo "NOK"
-(sudo cp neo4j_v4423.conf '$PATH/conf/neo4j.conf') && echo "OK" || echo "NOK"
-
-if [ cp "neo4j_v4423.conf" "$PATH/conf/neo4j.conf" ] ; then
-    echo "Command succeeded"
-else
-    echo "Command failed"
-fi
-# if [ "$foo" = "1" ] ; then
-#   foo=18
-# fi
-
-# sudo rm -Rf $PATH/data/databases/* $PATH/data/transactions/*
-# sudo neo4j console &
-# echo "You have reset your neo4j.conf setting"
+FILE_PATH=/Users/pro/neo4j-community-4.4.23
+echo "FILE_PATH: $FILE_PATH" && echo "OK" || echo "NOK"
+sudo cp neo4j_v4423.conf "$FILE_PATH/conf/neo4j.conf" && echo "OK" || echo "NOK"
+( sudo rm -Rf $FILE_PATH/data/databases/* $FILE_PATH/data/transactions/* ) && echo "OK" || echo "NOK"
+( sudo neo4j console & ) && echo "OK" || echo "NOK"
+( echo "You have reset your neo4j.conf setting" ) && echo "OK" || echo "NOK"
 
 
-if [ $? == 0 ] ; then
-  echo '<the output message you want to display>'
-else 
-  echo '<failure message>'
-fi
